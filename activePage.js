2b31229cd6bb2d9454761ffdda3e415fa44bf742
MonolinaS <monolinasarkar1@gmail.com>
2023-08-15 22:38:07 +0530

    Aug3

--- activePage.js
+++
@@ -1,9 +0,0 @@
-const activePage = window.location.pathname
-console.log(activePage)
-const navLinks = document.querySelectorAll('nav a')
-console.log(navLinks)
-navLinks.forEach(link=>{
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('active');
-    }
-})
